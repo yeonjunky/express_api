79a2c3314273026e6cb3f83f9e349a4748ca8a8e
yeonjun <yeonjunky@gmail.com>
2022-01-03 15:54:58 +0900

    middleware

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 import express from "express";
 import products from "./data.js";
+import { logger, auth } from "./middleWare.js";
 const app = express();
 
+// app.use(logger);
+app.use([auth, logger]); // multiple middleware can be use like this
+// the middleware will be executed in the same order as declare
+
 app.listen(8080, (req, res) => {
-  console.log("server is listening on port 8080");
+  console.log("server is listening on http://localhost:8080");
+});
+
+app.get("/", (req, res) => {
+  return "Hello world!";
 });
 
 app.get("/api/products", (req, res) => {
@@ -19,7 +28,7 @@ app.get("/api/products/:productID", (req, res) => {
   const product = products.find((product) => product.id === id);
 
   if (!product) {
-    return res.status(404).send("Produnt not found");
+    return res.status(404).send("Product not found");
   }
 
   res.json(product);
@@ -36,4 +45,8 @@ app.get("/api/query", (req, res) => {
   }
 
   res.json(products_result);
+});
+
+app.get("/about", (req, res) => {
+  return res.send("About page");
 });
---
+++ middleWare.js
@@ -0,0 +1,19 @@
+const logger = (req, res, next) => {
+  console.log(req.url);
+  console.log(req.params);
+  console.log(req.query);
+  next();
+};
+
+const auth = (req, res, next) => {
+  const user = req.query.user;
+
+  if (user === "admin") {
+    req.query.user = { name: "admin", id: 1 };
+    next();
+  } else {
+    res.status(401).send("Unauthorized");
+  }
+};
+
+export { logger, auth };
